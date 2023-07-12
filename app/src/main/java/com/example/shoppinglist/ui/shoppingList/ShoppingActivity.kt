4ec17110fc85f5fce524b3becf899bf6c054043a
AndriyKorzh <andriikorhz@gmail.com>
2023-07-12 19:23:06 +0300

    add basic realization for view model

--- app/src/main/java/com/example/shoppinglist/ui/shoppingList/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppingList/ShoppingActivity.kt
@@ -2,11 +2,19 @@ package com.example.shoppinglist.ui.shoppingList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.data.db.ShoppingDatabase
+import com.example.shoppinglist.data.repository.ShoppingRepository
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
+        val viewModel = ViewModelProvider(this, factory)[ShoppingViewModel::class.java]
     }
 }
