fc7681e4ff623b7579b3dc5d1bb8c15c4660fa20
AndriyKorzh <andriikorhz@gmail.com>
2023-07-13 22:57:13 +0300

    write main realization

--- app/src/main/java/com/example/shoppinglist/ui/shoppingList/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppingList/ShoppingActivity.kt
@@ -2,19 +2,51 @@ package com.example.shoppinglist.ui.shoppingList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.db.ShoppingDatabase
+import com.example.shoppinglist.data.db.entities.ShoppingItem
 import com.example.shoppinglist.data.repository.ShoppingRepository
+import com.example.shoppinglist.databinding.ActivityShoppingBinding
+import com.example.shoppinglist.other.ShoppingItemAdapter
 
 class ShoppingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityShoppingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        binding = ActivityShoppingBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
         val database = ShoppingDatabase(this)
         val repository = ShoppingRepository(database)
         val factory = ShoppingViewModelFactory(repository)
         val viewModel = ViewModelProvider(this, factory)[ShoppingViewModel::class.java]
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+
+        binding.rvShoppingList.layoutManager = LinearLayoutManager(this)
+        binding.rvShoppingList.adapter = adapter
+
+        viewModel.getAllShoppingItem().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        binding.fab.setOnClickListener {
+            AddShoppingItemDialog(this,
+            object : AddDialogListener {
+                override fun onButtonAddClicked(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+
+            }).show()
+        }
+
+
+
     }
 }
